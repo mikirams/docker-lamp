cb7039a3fc4043ab73342010091f30fde010e937
mikirams <132619513+mikirams@users.noreply.github.com>
2023-10-04 19:43:35 +0200

    zzz

--- www/crud_php/error.php
+++ www/crud_php/error.php
@@ -17,7 +17,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h1>Invalid Request</h1>
+                        <h1>Invalid  Request</h1>
                     </div>
                     <div class="alert alert-danger fade in">
                         <p>Sorry, you've made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>
